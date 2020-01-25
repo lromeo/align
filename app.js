5671b7dc348d1135f72b37f636432bdd82c9f5b0
Luke Romeo <luke@Kristins-iMac.fios-router.home>
2020-01-25 10:48:22 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express")
+var app = express()
+
+PORT = process.env.PORT || 3000
+IP = process.env.IP || "127.0. 0.1"
+
+app.set("view engine", "ejs")
+
+app.get("/", function(req, res){
+  res.render("home")
+})
+
+app.get("/about", function(req, res){
+  res.render("about")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+})
