921253e4c8809021c1870729f3a71e2bafe6b108
Luke Romeo <luke@Kristins-iMac.fios-router.home>
2020-01-28 22:21:55 -0500

    Contact form

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require("express")
 var app = express()
+const nodemailer = require("nodemailer");
+const bodyParser = require('body-parser')
 
-PORT = process.env.PORT || 3000
-IP = process.env.IP || "127.0. 0.1"
+app.use(bodyParser.urlencoded({extended: true}))
+
+if (process.env.NODE_ENV !== 'production') {
+  require("dotenv").config();
+}
 
 app.set("view engine", "ejs")
 app.use(express.static('public'))
@@ -15,6 +20,44 @@ app.get("/about", function(req, res){
   res.render("about")
 })
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.get("/contact", function(req, res){
+  res.render("contact")
+})
+
+const GMAIL_USER = process.env.GMAIL_USER
+const GMAIL_PASS = process.env.GMAIL_PASS
+const EMAIL_RECIPIENT = process.env.EMAIL_RECIPIENT
+
+console.log("GMAIL_USER", GMAIL_USER)
+
+app.post('/contact', (req, res) => {
+  const smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: GMAIL_USER,
+      pass: GMAIL_PASS
+    }
+  })
+
+  const mailOpts = {
+    from: 'Your sender info here',
+    to: EMAIL_RECIPIENT,
+    subject: 'New message from Align Scoliosis',
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+  }
+
+  smtpTrans.sendMail(mailOpts, (error, response) => {
+    if (error) {
+      console.log(error)
+    } else {
+      res.render('contact_success')
+    }
+  })
+
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
 })
